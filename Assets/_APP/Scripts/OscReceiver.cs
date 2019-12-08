3bdcc399379cd6c34af22f2d1912b8eb446e812e
SMARTIUM <dev@smartium.io>
2019-12-07 23:04:49 -0300

    First commit.

---
+++ Assets/_APP/Scripts/OscReceiver.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using OscJack;
+
+class OscReceiver : MonoBehaviour
+{
+    public Text txtTimer;
+    
+    IEnumerator Start()
+    {
+        var server = new OscServer(7000); // Port number
+
+        server.MessageDispatcher.AddCallback(
+            "/addminute", // OSC address
+            (string address, OscDataHandle data) =>
+            {
+                txtTimer.text = "test";
+                print(txtTimer.text);
+            }
+        );
+        
+        server.MessageDispatcher.AddCallback(
+            "/subminute", // OSC address
+            (string address, OscDataHandle data) => {
+                Debug.Log(string.Format("({0}, {1})",
+                    data.GetElementAsFloat(0),
+                    data.GetElementAsFloat(1)));
+            }
+        );
+
+        //yield return new WaitForSeconds(10);
+        yield break;
+        //server.Dispose();
+    }
+}
---
+++ Assets/_APP/Scripts/Timer.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using OscJack;
+
+public class Timer : MonoBehaviour
+{
+    public Text txtCounter;
+    public int time;
+    
+    public GameObject inputTime;
+    public Button btnConfirm;
+    public Button btnStart;
+
+    private int counter;
+    private bool isPlaying = false;
+    private Coroutine counterRoutine;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       counter = time;
+
+       StartCoroutine(OscListener());
+        
+        //inputTime.GetComponentInChildren<Text>().text = "test";
+        time *= 60;
+        
+        btnStart.onClick.AddListener(delegate
+        {
+            if (isPlaying)
+            {
+                stopTimer();
+            }
+            else
+            {
+                if (counter < 1)
+                {
+                    stopTimer("reset");
+                }
+                else
+                {
+                    startTimer(time);    
+                }
+            }
+
+            isPlaying = !isPlaying;
+        });
+        
+        btnConfirm.onClick.AddListener(delegate
+        {
+            time = int.Parse(inputTime.GetComponentInChildren<Text>().text)*60;
+            //txtCounter.text = "";
+            counter = time;
+            setCounterDisplay();
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlaying)
+        {
+            setCounterDisplay();
+        }
+    }
+    
+    IEnumerator Countdown (int seconds) {
+        counter = seconds;
+        while (counter > -1) {
+            yield return new WaitForSeconds (1);
+            //print(counter);
+            if (counter <= Math.Round(time*0.2f)+1 && counter > Math.Round(time*0.1f))
+            {
+               txtCounter.color = Color.yellow;
+            }
+            
+            else if (counter <= Math.Round(time*0.1f)+1)
+            {
+                txtCounter.color = Color.red;
+            }
+            counter--;
+        }
+        isPlaying = !isPlaying;
+        btnStart.GetComponentInChildren<Text>().text = "PREPARAR";
+        btnStart.GetComponentInChildren<Text>().color = Color.black;
+        btnStart.image.color = Color.yellow;
+        txtCounter.text = "FIM";
+    }
+    
+    IEnumerator OscListener()
+    {
+        var server = new OscServer(8000); // Port number
+
+        server.MessageDispatcher.AddCallback(
+            "/addminute", // OSC address
+            (string address, OscDataHandle data) =>
+            {
+                AddMinute();
+            }
+        );
+        
+        server.MessageDispatcher.AddCallback(
+            "/subminute", // OSC address
+            (string address, OscDataHandle data) => {
+               RemoveMinute();
+            }
+        );
+        yield break;
+    }
+
+    void AddMinute()
+    {
+        counter++;
+    }
+    
+    void RemoveMinute()
+    {
+        counter--;
+    }
+
+    void startTimer(int _time)
+    {
+        txtCounter.color = Color.green;
+        counterRoutine = StartCoroutine(Countdown(_time));
+        btnStart.GetComponentInChildren<Text>().text = "PARAR";
+        btnStart.GetComponentInChildren<Text>().color = Color.white;
+        inputTime.SetActive(false);
+        btnConfirm.gameObject.SetActive(false);
+        btnStart.image.color = Color.red;
+    }
+
+    void stopTimer(string _reset = "no")
+    {
+        if (_reset != "reset")
+        {
+            StopCoroutine(counterRoutine);
+        }
+        btnStart.GetComponentInChildren<Text>().text = "INICIAR";
+        btnStart.GetComponentInChildren<Text>().color = Color.black;
+        btnStart.image.color = Color.green;
+        counter = time;
+        inputTime.SetActive(true);
+        btnConfirm.gameObject.SetActive(true);
+    }
+
+    void setCounterDisplay()
+    {
+        int hours = counter / 60 / 60;
+        int minutes = counter / 60 % 60;
+        int seconds = counter % 60;
+        string strMinutes = null;
+        string strSeconds = null;
+ 
+        if(minutes < 10) {
+            strMinutes = "0" + minutes;
+        }
+        else
+        {
+            strMinutes = minutes.ToString();
+        }
+        if(seconds < 10) {
+            strSeconds = "0" + seconds;
+        }
+        else
+        {
+            strSeconds = seconds.ToString();
+        }
+        txtCounter.text = hours + ":" + strMinutes + ":" + strSeconds;
+    }
+}
