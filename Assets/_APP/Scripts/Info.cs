9a88241cee2e5a292a9202812e3d1cd7f858c081
SMARTIUM <developer@smartium.app>
2019-12-09 23:10:59 -0300

    Imported Info button image; Created Info button; Created Info script; Added Info script to MANAGER GameObject; Assigned Info image and Info Button to Info script.

---
+++ Assets/_APP/Scripts/Info.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour
+{
+    public Button btnInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnInfo.onClick.AddListener(delegate
+        {
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
