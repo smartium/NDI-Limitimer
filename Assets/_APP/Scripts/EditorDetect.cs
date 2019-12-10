4c6f71e7f04e09acfe04f6bc2d8df32eee24fbad
SMARTIUM <developer@smartium.app>
2019-12-10 00:07:10 -0300

    Created script to detect editor. If false the mockup panel is hidded.

---
+++ Assets/_APP/Scripts/EditorDetect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EditorDetect : MonoBehaviour
+{
+    public GameObject pnlMockup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Application.isEditor)
+        {
+            pnlMockup.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
